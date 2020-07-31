a4634dd5b4527408be781793dd2aff50c6d8f9ed
kp1129 <katya.p.1129@gmail.com>
2020-07-30 20:22:45 -0700

    add cell toggle

--- app/src/App.js
+++ app/src/App.js
@@ -10,24 +10,30 @@ const App = () => {
   const numCols = 10;
 
   const emptyGrid = () => {
-    const outer_array = []
+    const outer_array = [];
     for(let i = 0; i < numRows; i++){
-      const inner_array = []
+      const inner_array = [];
       for(let j = 0; j < numCols; j++){
         inner_array.push(0);
       }
       outer_array.push(inner_array);
     }
-    console.log(outer_array);
     return outer_array;
   }
 
   const [grid, setGrid] = useState(emptyGrid());
 
+  const handleCellClick = (x, y) => {
+    // toggle cell value between 0 and 1
+    setGrid([...grid], grid[x][y] = (grid[x][y] === 0 ? 1 : 0));
+  }
 
   return (
   <div style={{display: "grid", gridTemplateColumns: `repeat(${numCols}, 25px)`}}>
-    {grid.map(array => array.map(cell => <div style={{border: "1px solid black", width: "25px", height: "25px"}}></div>))}
+    {grid.map((arr, i) => {
+      
+      return arr.map((cell, j) => {
+      return <div key={`${i}--${j}`} onClick={() => handleCellClick(i, j)} style={{border: "1px solid black", width: "25px", height: "25px", backgroundColor: grid[i][j] === 1 ? "indianred" : ""}}/>})})}
   </div>
   );
 }
